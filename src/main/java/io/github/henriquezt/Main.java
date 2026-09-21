ecc2b414b03cf2378db6e5d6c27a495738f70d73
Henrique Zanchetta <henriqueztiozzo@gmail.com>
2026-09-21 12:02:25 -0300

    Seller entity class

--- src/main/java/io/github/henriquezt/Main.java
+++ src/main/java/io/github/henriquezt/Main.java
@@ -1,10 +1,16 @@
 package io.github.henriquezt;
 
 import Model.entities.Department;
+import Model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        IO.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        IO.println(seller);
     }
 }
