928cf39202cb2de89d5fef746492de593fa0adac
Henrique Zanchetta <henriqueztiozzo@gmail.com>
2026-09-21 11:48:39 -0300

    Department entity class

--- src/main/java/io/github/henriquezt/Main.java
+++ src/main/java/io/github/henriquezt/Main.java
@@ -1,7 +1,10 @@
 package io.github.henriquezt;
 
+import Model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-
+        Department obj = new Department(1, "Books");
+        IO.println(obj);
     }
 }
